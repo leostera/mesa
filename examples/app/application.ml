c3838afa77fd65c3883af3c332e906c48d1b7521
Leandro Ostera <leandro@abstractmachines.dev>
2023-11-16 06:48:17 +0100

    chore: fmt

--- examples/app/application.ml
+++ examples/app/application.ml
@@ -3,7 +3,5 @@ open Riot
 let name = "Test_application"
 
 let start () =
-  let child_specs = [
-    App_web.Endpoint.child_spec { port = 2112 };
-  ] in
+  let child_specs = [ App_web.Endpoint.child_spec { port = 2112 } ] in
   Supervisor.start_link ~child_specs ()
